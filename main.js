c5ae0eddeefc9a960bd6ce214a6177e44867fc46
MarwaneQ <merouane12345bensadallah@gmail.com>
2023-12-18 09:31:57 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+function clock() {
+    let hours = document.getElementById('hours')
+    let minute = document.getElementById('minutes')
+    let seconds = document.getElementById('seconds')
+    let ampm = document.getElementById('ampm')
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    var am = 'AM'
+    if (h > 12) {
+        h = h - 12
+        var am = 'PM'
+        
+    }
+    h = (h < 10) ? '0' + h : h;
+    m = (m < 10) ? '0' + m : m;
+    s = (s < 10) ? '0' + s : s;
+    hours.innerHTML = h;
+    minute.innerHTML = m;
+    seconds.innerHTML = s;
+}
+var interval=setInterval(clock,1000)
