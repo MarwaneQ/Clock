c5336eef64bfb35993980f2921082cc59af62bca
MarwaneQ <merouane12345bensadallah@gmail.com>
2023-12-21 13:27:36 -0800

    remove pm am

--- main.js
+++ main.js
@@ -6,10 +6,8 @@ function clock() {
     let h = new Date().getHours()
     let m = new Date().getMinutes()
     let s = new Date().getSeconds()
-    var am = 'AM'
     if (h > 12) {
         h = h - 12
-        var am = 'PM'
         
     }
     h = (h < 10) ? '0' + h : h;
