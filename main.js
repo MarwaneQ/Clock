fd51dc7a96ea43af6bbab6f325d6ff28ba49a6c2
SamiSelx <samifcb14@gmail.com>
2023-12-21 22:53:43 -0800

    AmPm

--- main.js
+++ main.js
@@ -6,15 +6,18 @@ function clock() {
     let h = new Date().getHours()
     let m = new Date().getMinutes()
     let s = new Date().getSeconds()
+    let am = 'AM'
     if (h > 12) {
         h = h - 12
+        am = 'PM'
         
     }
-    h = (h < 10) ? '0' + h : h;
-    m = (m < 10) ? '0' + m : m;
-    s = (s < 10) ? '0' + s : s;
-    hours.innerHTML = h;
-    minute.innerHTML = m;
-    seconds.innerHTML = s;
+
+    hours.innerHTML = h < 10 ? '0'+ h : h;
+    minute.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+    ampm.innerHTML = am;
+
 }
 setInterval(clock,1000)
+
